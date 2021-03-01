21b69c917fbecd34b7a55737a94349bbd5f8aae2
Choidaeboem <chleoqja0125@nate.com>
2021-03-01 11:45:47 +0900

    add webpack

---
+++ frontend/webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+
+module.exports = {
+  context: __dirname,
+  entry: './src/index.tsx',
+  module: {
+    rules: [
+      {
+        test: /\.ts|\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|j?g|svg|gif)?$/,
+        use: 'file-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: path.join(__dirname, 'build'),
+    compress: true,
+    port: 9000,
+  },
+
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'build'),
+    // publicPath: '/',
+  },
+  plugins: [
+    new CleanWebpackPlugin({cleanStaleWebpackAssets: false}),
+    new HtmlWebPackPlugin({
+      template: path.resolve(__dirname, 'src/index.html'),
+      filename: 'index.html',
+    }),
+  ],
+}
