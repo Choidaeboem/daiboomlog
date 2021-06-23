24be92a7d2f983fecafa617ec3928f38381f074c
Choidaeboem <chleoqja0125@nate.com>
2021-06-24 01:57:48 +0900

    divide

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, {FunctionComponent, ReactChild} from 'react'
+import {Layout} from 'antd'
 
-const App = () => {
-  return <div>Hello, Daiboom Blog</div>
+import SideBar from './Sidebar'
+
+interface Props {
+  children: ReactChild
+}
+
+const App: FunctionComponent<Props> = ({children}) => {
+  const {Footer} = Layout
+
+  return (
+    <Layout style={{minHeight: '100vh'}}>
+      <SideBar />
+      <Layout className="site-layout">
+        {children}
+        <Footer style={{textAlign: 'center'}}>
+          Ant Design ©2018 Created by Ant UED
+        </Footer>
+      </Layout>
+    </Layout>
+  )
 }
 
 export default App
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,13 +1,49 @@
 import React from 'react'
 import {render} from 'react-dom'
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
 
 // Utils
 import App from './App'
+import Editor from './editor'
+
+import './style/daiboomlog.scss'
 
 const rootNode = document.querySelector('#react-root')
 
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  )
+}
+
+function Dashboard() {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+    </div>
+  )
+}
+
 const Root = () => {
-  return <App />
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <App>
+            <Editor />
+          </App>
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/dashboard">
+          <Dashboard />
+        </Route>
+      </Switch>
+    </Router>
+  )
 }
 
 if (rootNode) {
